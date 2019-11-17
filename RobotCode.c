7544b156e596150928ee00f7e6f5b6b3c15c0665
Cindy Li <cindyli0213@hotmail.com>
2019-11-17 13:57:09 -0500

    More testing

--- RobotCode.c
+++ RobotCode.c
@@ -24,7 +24,7 @@
  * 		0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	15
  * 					  a b c d e f  g   h
  */
- 
+
 // it is declared that an empty spot on the chess board is represented by null values
 
 // chess piece type
@@ -32,6 +32,7 @@ struct piece
 {
 	int piece_type;
 	float extend_dist;
+	float close_dist;
 	bool colour; // true = white, false = black
 };
 
@@ -39,22 +40,27 @@ struct piece
 piece board[16][8];
 
 // physical system constants
-const float WHEEL_RADIUS_X = 3.7;
-const float WHEEL_RADIUS_Y = 1.8;
+const float WHEEL_RADIUS_X = 3.65;
+const float WHEEL_RADIUS_Y = 1.65;
 const float TILE_SIDE = 3;
-const float EXTEND_DIST_Z = 5.3;
-const float EXTEND_DIST_CLAW = 2.3;
+const float EXTEND_DIST_Z = 5.2;
+const float EXTEND_DIST_CLAW = 2.5;
 const float DIST_PER_ROTATION_X = WHEEL_RADIUS_X*PI*2/5;
 const float DIST_PER_ROTATION_Y = WHEEL_RADIUS_Y*PI*2;
 const float DIST_PER_ROTATION_Z = 3.6;
 const float DIST_PER_ROTATION_CLAW = 0.5;
+
+const float MOTOR_X_SPEED = 60;
+const float MOTOR_X_SPEED_NEG = -60;
+const float MOTOR_Y_SPEED = 60;
+const float MOTOR_Z_SPEED = 40;
 
 const float PAWN_EXTEND_DIST = EXTEND_DIST_Z;
 const float PAWN_CLOSE_DIST = 0;
 const float ROOK_EXTEND_DIST = EXTEND_DIST_Z;
 const float ROOK_CLOSE_DIST = 1.5;
 const float KNIGHT_EXTEND_DIST = EXTEND_DIST_Z;
-const float KNIGHT_CLOSE_DIST = 0.4;
+const float KNIGHT_CLOSE_DIST = 0.1;
 const float BISHOP_EXTEND_DIST = EXTEND_DIST_Z;
 const float BISHOP_CLOSE_DIST = 1.2;
 const float QUEEN_EXTEND_DIST = EXTEND_DIST_Z - 1;
@@ -69,6 +75,7 @@ const int KNIGHT = 2;
 const int BISHOP = 3;
 const int QUEEN = 4;
 const int KING = 5;
+const int NULL_PIECE = 6;
 
 // motors
 const int x_motor1 = mmotor_S1_1;
@@ -85,23 +92,33 @@ const int touch_y = S3;
  *
  * CINDY
  */
-void move_x(int x)
+void move_x(int x, int offset)
 {
-	if(MSMMotorEncoder(x_motor1)/360.0*DIST_PER_ROTATION_X < x*TILE_SIDE + 1.6)
+	if(MSMMotorEncoder(x_motor1)/360.0*DIST_PER_ROTATION_X < x*TILE_SIDE + 1.5 + offset)
 	{
-		MSMMotor(x_motor1, 40);
-		MSMMotor(x_motor2, 40);
-		while(MSMMotorEncoder(x_motor1)/360.0*DIST_PER_ROTATION_X < x*TILE_SIDE + 1.6) {}
+		while(MSMMotorEncoder(x_motor1)/360.0*DIST_PER_ROTATION_X < x*TILE_SIDE + 1.5 + offset)
+		{
+			MSMMotor(x_motor1, MOTOR_X_SPEED);
+			MSMMotor(x_motor2, MOTOR_X_SPEED);
+			while(MSMMotorEncoder(x_motor1)/360.0*DIST_PER_ROTATION_X < x*TILE_SIDE + 1.5 + offset) {}
+			MSMotorStop(x_motor1);
+			MSMotorStop(x_motor2);
+			wait1Msec(100);
+		}
 	}
 	else
 	{
-		MSMMotor(x_motor1, -40);
-		MSMMotor(x_motor2, -40);
-		while(MSMMotorEncoder(x_motor1)/360.0*DIST_PER_ROTATION_X > x*TILE_SIDE + 1.6) {}
+		while(MSMMotorEncoder(x_motor1)/360.0*DIST_PER_ROTATION_X > x*TILE_SIDE + 2 + offset)
+		{
+			MSMMotor(x_motor1, MOTOR_X_SPEED_NEG);
+			MSMMotor(x_motor2, MOTOR_X_SPEED_NEG);
+			while(MSMMotorEncoder(x_motor1)/360.0*DIST_PER_ROTATION_X > x*TILE_SIDE + 2 + offset) {}
+			MSMotorStop(x_motor1);
+			MSMotorStop(x_motor2);
+			wait1Msec(100);
+		}
 	}
-	MSMotorStop(x_motor1);
-	MSMotorStop(x_motor2);
-	wait1Msec(500);
+	wait1Msec(200);
 }
 
 /*
@@ -113,16 +130,16 @@ void move_y(int y)
 {
 	if(nMotorEncoder(y_motor)/360.0*DIST_PER_ROTATION_Y > -y*TILE_SIDE - 0.7)
 	{
-		motor[y_motor] = -20;
+		motor[y_motor] = -MOTOR_Y_SPEED;
 		while(nMotorEncoder(y_motor)/360.0*DIST_PER_ROTATION_Y > -y*TILE_SIDE - 0.7) {}
 	}
 	else
 	{
-		motor[y_motor] = 20;
+		motor[y_motor] = MOTOR_Y_SPEED;
 		while(nMotorEncoder(y_motor)/360.0*DIST_PER_ROTATION_Y < -y*TILE_SIDE - 0.7) {}
 	}
 	motor[y_motor] = 0;
-	wait1Msec(500);
+	wait1Msec(200);
 }
 
 /*
@@ -132,14 +149,14 @@ void move_y(int y)
  */
 void calibrate_x()
 {
-	MSMMotor(x_motor1, -40);
-	MSMMotor(x_motor2, -40);
+	MSMMotor(x_motor1, MOTOR_X_SPEED_NEG);
+	MSMMotor(x_motor2, MOTOR_X_SPEED_NEG);
 	while (SensorValue[touch_x] == 0) {}
 	MSMotorStop(x_motor1);
 	MSMotorStop(x_motor2);
 	MSMMotorEncoderReset(x_motor1);
 	MSMMotorEncoderReset(x_motor2);
-	wait1Msec(500);
+	wait1Msec(200);
 }
 
 /*
@@ -149,11 +166,11 @@ void calibrate_x()
  */
 void calibrate_y()
 {
-	motor[y_motor] = 30;
+	motor[y_motor] = MOTOR_Y_SPEED;
 	while (SensorValue[touch_y] == 0) {}
 	motor[y_motor] = 0;
 	nMotorEncoder(y_motor) = 0;
-	wait1Msec(500);
+	wait1Msec(200);
 }
 
 /*
@@ -161,9 +178,10 @@ void calibrate_y()
  *
  * CINDY
  */
-void moveArm(int x, int y)
+void moveArm(int x, int y, int offset_x)
 {
-	move_x(x);
+	move_x(x, offset_x);
+	calibrate_y();
 	move_y(y);
 }
 
@@ -177,7 +195,7 @@ void closeClaw(int dist)
 	motor[claw_motor] = -100;
 	while(nMotorEncoder(claw_motor)/360.0*DIST_PER_ROTATION_CLAW > dist) {}
 	motor[claw_motor] = 0;
-	wait1Msec(500);
+	wait1Msec(200);
 }
 
 /*
@@ -190,7 +208,7 @@ void openClaw()
 	motor[claw_motor] = 100;
 	while(nMotorEncoder(claw_motor)/360.0*DIST_PER_ROTATION_CLAW < EXTEND_DIST_CLAW) {}
 	motor[claw_motor] = 0;
-	wait1Msec(500);
+	wait1Msec(200);
 }
 
 /*
@@ -200,10 +218,10 @@ void openClaw()
  */
 void lowerClaw(int dist)
 {
-	motor[z_motor] = -40;
+	motor[z_motor] = -MOTOR_Z_SPEED;
 	while(nMotorEncoder(z_motor)/360.0*DIST_PER_ROTATION_Z > -dist) {}
 	motor[z_motor] = 0;
-	wait1Msec(500);
+	wait1Msec(200);
 }
 
 /*
@@ -213,11 +231,23 @@ void lowerClaw(int dist)
  */
 void raiseClaw()
 {
-	motor[z_motor] = 40;
+	motor[z_motor] = MOTOR_Z_SPEED;
 	while(nMotorEncoder(z_motor)/360.0*DIST_PER_ROTATION_Z < 0) {}
 
 	motor[z_motor] = 0;
-	wait1Msec(500);
+	wait1Msec(200);
+}
+
+/*
+ * Removes a piece from the chess board array.
+ *
+ * ALEX
+ */
+void removeFromArray(int x, int y)
+{
+	piece null_piece;
+	null_piece.piece_type = NULL_PIECE;
+	board[x][y] = null_piece;
 }
 
 /*
@@ -228,22 +258,94 @@ void raiseClaw()
 void movePiece(int x_start, int y_start, int x_end, int y_end, piece & currPiece)
 {
 	calibrate_y();
+	move_y(7);
 	calibrate_x();
-	moveArm(int x_start, int y_start);
-	lowerClaw(); // might need piece_type in the future
-	closeClaw(); //same
-	raiseClaw(); //same
-	moveArm(int x_end, int y_end);
-	lowerClaw(); // might need piece_type in the future
-	openClaw(); //same
-	raiseClaw(); //same
+	moveArm(x_start, y_start, 0);
+	lowerClaw(currPiece.extend_dist);
+	closeClaw(currPiece.close_dist);
+	raiseClaw();
+	moveArm(x_end, y_end, - 0.8);
+	lowerClaw(currPiece.extend_dist);
+	openClaw();
+	raiseClaw();
 	board[x_end][y_end] = currPiece;
 	removeFromArray(x_start, y_start);
 }
 
-void removeFromArray(int x, int y)
+//subfunction of removePiece()
+void holdingSpot(piece & currPiece, int & x, int & y,
+ 	int pawnColumn, int pieceColumn, int queenColumn)
 {
-	board[x][y] = null;
+	// look through the array of it's type, find the last available location for it
+	if(currPiece.piece_type == PAWN)
+	{
+		for (int row = 0; row <= 7; row++)// search pawnColumn
+		{
+			if(board[pawnColumn][row].piece_type == NULL_PIECE)
+			{
+				x = pawnColumn;
+				y = row;
+			}
+		}
+	}
+	else if(currPiece.piece_type == ROOK)
+	{
+		if(board[pieceColumn][0].piece_type == NULL_PIECE)
+		{
+			x = pieceColumn;
+			y = 0;
+		}
+		else
+		{
+			x = pieceColumn;
+			y = 7;
+		}
+	}
+	else if(currPiece.piece_type == KNIGHT)
+	{
+		if(board[pieceColumn][1].piece_type == NULL_PIECE)
+		{
+			x = pieceColumn;
+			y = 1;
+		}
+		else
+		{
+			x = pieceColumn;
+			y = 6;
+		}
+	}
+	else if(currPiece.piece_type == BISHOP)
+	{
+		if(board[pieceColumn][2].piece_type == NULL_PIECE)
+		{
+			x = pieceColumn;
+			y = 2;
+		}
+		else
+		{
+			x = pieceColumn;
+			y = 5;
+		}
+	}
+	else if(currPiece.piece_type == QUEEN)
+	{
+		if(board[pieceColumn][3].piece_type == NULL_PIECE)
+		{
+			x = pieceColumn;
+			y = 3;
+		}
+		else
+		{
+			for (int row = 0; row <= 7; row++)  // search queenColumn
+			{
+				if(board[queenColumn][row].piece_type == NULL_PIECE)
+				{
+					x = queenColumn;
+					y = row;
+				}
+			}
+		}
+	}
 }
 
 /*
@@ -254,7 +356,7 @@ void removeFromArray(int x, int y)
 void removePiece(int x, int y, piece & currPiece)
 {
 	int endx = 0, endy = 0; // endpoint of piece
-	
+
 	if(currPiece.colour)// find if piece is white
 	{
 		//search columns 13-15
@@ -263,44 +365,9 @@ void removePiece(int x, int y, piece & currPiece)
 	else
 	{
 		//search columns 0-2
-		holdingSpot(currPiece, endx, endy, 2, 1, 0)	
+		holdingSpot(currPiece, endx, endy, 2, 1, 0)
 	}
 	movePiece(x, y, endx, endy, currPiece)
-}
-//subfunction of removePiece()
-void holdingSpot(piece & currPiece, int & x, int & y,
- int pawnColumn, int pieceColumn, int queenColumn)
-{// look through the array of it's type, find the last available location for it
-	if(currPiece.piece_type = PAWN) 
-		for (int row = 0; row <= 7, row++)// search pawnColumn
-		{
-			if(board[pawnColumn][row] == null)
-				x = pawnColumn, y = row;
-		}
-	else if(currPiece.piece_type = ROOK)
-		if(board[pieceColumn][0] == null)
-			x = pieceColumn, y = 0;
-		if(board[pieceColumn][7] == null)
-			x = pieceColumn, y = 7;
-	else if(currPiece.piece_type = KNIGHT)
-		if(board[pieceColumn][1] == null)
-			x = pieceColumn, y = 1;
-		if(board[pieceColumn][6] == null)
-			x = pieceColumn, y = 6;
-	else if(currPiece.piece_type = BISHOP)
-		if(board[pieceColumn][2] == null)
-			x = pieceColumn, y = 2;
-		if(board[pieceColumn][5] == null)
-			x = pieceColumn, y = 5;
-	else if(currPiece.piece_type = QUEEN)
-		if(board[pieceColumn][3] == null)
-			x = pieceColumn, y = 3;
-		for (int row = 0; row <= 7, row++)  // search queenColumn
-		{
-			if(board[queenColumn][row] == null)
-				x = queenColumn, y = row;
-		}
-	// movePiece(location)
 }
 
 /*
@@ -326,36 +393,36 @@ int check(bool white)
 }
 
 /*
- * Returns the corresponding number to a letter.
- * A = 0
- * B = 1
- * C = 2
- * D = 3
- * E = 4
- * F = 5
- * G = 6
- * H = 7
+ * Returns the corresponding x coordinate to a letter.
+ * A = 4
+ * B = 5
+ * C = 6
+ * D = 7
+ * E = 8
+ * F = 9
+ * G = 10
+ * H = 11
  *
  * CINDY
  */
-int num(char letter)
+int x_coord(char letter)
 {
 	if (letter == 'A')
-		return 0;
-	else if (letter == 'B')
-		return 1;
-	else if (letter == 'C')
-		return 2;
-	else if (letter == 'D')
-		return 3;
-	else if (letter == 'E')
 		return 4;
-	else if (letter == 'F')
+	else if (letter == 'B')
 		return 5;
-	else if (letter == 'G')
+	else if (letter == 'C')
 		return 6;
-	else if (letter == 'H')
+	else if (letter == 'D')
 		return 7;
+	else if (letter == 'E')
+		return 8;
+	else if (letter == 'F')
+		return 9;
+	else if (letter == 'G')
+		return 10;
+	else if (letter == 'H')
+		return 11;
 	else
 		return -1;
 }
@@ -447,17 +514,16 @@ task main()
 	nMotorEncoder(claw_motor) = 0;
 
 	// testing
-	calibrate_x();
-	move_x(6);
-	calibrate_y();
-	move_y(6);
+	piece test;
+	test.piece_type = PAWN;
+	test.extend_dist = PAWN_EXTEND_DIST;
+	test.close_dist = PAWN_CLOSE_DIST;
+	test.colour = true;
+
 	openClaw();
-	lowerClaw(PAWN_EXTEND_DIST);
-	closeClaw(PAWN_CLOSE_DIST);
-	raiseClaw();
-	move_y(4);
-	lowerClaw(PAWN_EXTEND_DIST);
-	openClaw();
-	raiseClaw();
+	for(int i = 4; i < 12; i++)
+	{
+		movePiece(i, 6, i, 4, test);
+	}
 	closeClaw(0);
 }
